1432e618a6982f673777cbe339fd9062ba2a5dc9
Valentino <Valentino@VALEN-PC>
2018-01-24 00:19:33 -0300

    All are ready!

---
+++ Tube/Tube-Core/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <SDL2\SDL.h>
+
+int main(int argc, char *args[])
+{
+	std::cout << "An old tube can be a game engine?" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
